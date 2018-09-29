18ef5b280f4451e843db31d224462cfd9d8d332d
zhang <zhang@miubiq.cs.titech.ac.jp>
2018-09-29 22:38:29 +0900

    complete atcoder ABV 111 at 09/29, 3 AC, 1 didn't complete

---
+++ src/AtcoderContest0929/test4.java
@@ -0,0 +1,18 @@
+package AtcoderContest0929;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class test4 {
+
+  public static void main(String[] args) throws FileNotFoundException {
+
+
+
+  }
+
+
+}
