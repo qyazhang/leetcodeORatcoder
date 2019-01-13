540e664ac487104edeef81e2bcb8f870d8b07d76
qyazhang <qyazhang1995@gmail.com>
2019-01-13 20:30:33 +0900

    add atcoder 20180902 code

--- src/leetcode/leetcodeContest0902/test1.java
+++ src/leetcode/leetcodeContest0902/test1.java
@@ -1,41 +1,39 @@
 package leetcodeContest0902;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class test1 {
 
     public static void main(String[] args) {
 
-        int[] A = {1,2,4,5};
-        boolean res = isMonotonic(A);
-        System.out.println(res);
+
     }
 
-    public static boolean isMonotonic(int[] A) {
+    public String reverseOnlyLetters(String S) {
 
-        int min = -100001;
-        int max = 100001;
-        int mark1 = 0;
-        int mark2 = 0;
-        for (int i=1;i<A.length;i++) {
-            if (A[i]>=A[i-1]) continue;
-            else {
-                mark1 = 1;
-                break;
+        char[] in = S.toCharArray();
+        char[] res = S.toCharArray();
+
+        int pos1 = 0;
+        int pos2 = S.length()-1;
+        while (pos1<S.length()&&pos2>=0) {
+            System.out.println(in[pos1]+" "+res[pos2]);
+            if (!isChar(in[pos1])) pos1++;
+            else if (!isChar(res[pos2])) pos2--;
+            else if (isChar(in[pos1]) && isChar(res[pos2])) {
+                res[pos2] = in[pos1];
+                pos1++;
+                pos2--;
             }
         }
+        return String.valueOf(res);
 
-        for (int i=1;i<A.length;i++) {
-            if (A[i]<=A[i-1]) continue;
-            else {
-                mark2 = 1;
-                break;
-            }
-        }
+    }
 
-        if (mark1==0||mark2==0) return true;
+    public boolean isChar(char c) {
+        if ((c>='a'&&c<='z')||(c>='A'&&c<='Z')) return true;
         else return false;
-
     }
 
 }
